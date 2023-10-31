ed236bc369420d25d543c18fdd5eb85124958a90
Abarna Shanmugam <98462784+abarna-24@users.noreply.github.com>
2023-10-31 10:37:50 +0530

    Add files via upload

---
+++ lib/consts/strings.dart
@@ -0,0 +1,6 @@
+const String degree = "°";
+const String apiKey = "154d4df89380a03b11b25329fb470d9a";
+const String currentEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String daysEndpoint = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+const String latitude="11.4102";
+const String longitude="76.6950";
